05c1429940c7f3eae5ded2cb25fa566b342b9a48
Ernest Alilovic <ernest.alilovic@gmail.com>
2018-06-01 16:27:48 +0200

    Defined player

--- js/app.js
+++ js/app.js
@@ -6,18 +6,18 @@ var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
 };
 
-
 Enemy.prototype.update = function(dt) {
     this.x =+ this.speed * dt;
     if (this.x > 510) {
       this.x = -50;
       this.speed = 100 + Math.floor(Math.random() * 300);
     }
-
+// Defines collision between enemy and player
     if (player.x < this.x + 80 &&
         player.x + 80 > this.x &&
         player.y < this.y + 60 &&
         60 + player.y > this.y) {
+// In case of collision, player gets reset to starting position
         player.x = 202;
         player.y = 405,
     }
@@ -27,9 +27,40 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var Player = function (x, y) {
+    this.x = x;
+    this.y = y;
+    this.player = 'images/char-princess-girl.png'
+}
+
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.player), this.x, this.y);
+}
+
+Player.prototype.handleInput = function (keyPress) {
+    if (keyPress == 'left' && this.x > 0) {
+      this.x -= 102;
+    }
+    if (keyPress == 'right' && this.x < 405) {
+      this.x += 102;
+    }
+    if (keyPress == 'up' && this.y > 0) {
+      this.y -= 83;
+    }
+    if (keyPress == 'down' && this.y < 405) {
+      this.y += 83;
+    }
+    if (this.y < 0) {
+      setTimeout(function() {
+        player.x = 202;
+        player.y = 405;
+      }, 500);
+    }
+}
 
 
 // Now instantiate your objects.
